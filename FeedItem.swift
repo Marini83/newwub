f644aa4e5ee0bd78ba6919dcf5ef9f5346459b3f
marin <kallashi@gmail.com>
2015-02-18 22:28:54 -0500

    first commit

--- FeedItem.swift
+++ FeedItem.swift
@@ -2,20 +2,22 @@
 //  FeedItem.swift
 //  Wub
 //
-//  Created by User  on 2015-01-13.
+//  Created by User  on 2015-02-16.
 //  Copyright (c) 2015 Wub.com. All rights reserved.
 //
 
 import Foundation
 import CoreData
 
-
-@objc (FeedItem)
 class FeedItem: NSManagedObject {
-    
+
     @NSManaged var caption: String
+    @NSManaged var cdate: NSNumber
     @NSManaged var image: NSData
     @NSManaged var thumbNail: NSData
-    @NSManaged var cdate: NSTimeInterval
-    
+    @NSManaged var latitude: NSNumber
+    @NSManaged var longitude: NSNumber
+    @NSManaged var uniqueID: String
+    @NSManaged var filtered: NSNumber
+
 }
